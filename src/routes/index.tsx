842d4c28eaa94f257bf26f220817bcef5e90aab1
Matheus Gomes <mth.gom@gmail.com>
2022-05-17 18:35:21 -0300

    Forgot password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import checkRoute from './checkRoute';
-import SignIn from '../pages/signin';
-import SignUp from '../pages/signup';
+import SignIn from '../pages/Signin';
+import SignUp from '../pages/Signup';
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 const Router: React.FC = () => (
   <Routes>
     <Route path="/" element={checkRoute(SignIn)} />
     <Route path="/signup" element={checkRoute(SignUp)} />
     <Route path="/dashboard" element={checkRoute(Dashboard, true)} />
+    <Route path="/forgot-password" element={checkRoute(ForgotPassword)} />
   </Routes>
 );
 
